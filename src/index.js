8d616a019704448bf0f7ca6e478aa7c25899d0b3
Torrence Brown <torrenceb90@gmail.com>
2021-12-09 23:05:45 -0700

    Wrote store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+
+// Action
+const increment = () => ({
+  type: "INCREMENT",
+});
+
+const decrement = () => ({
+  type: "DECREMENT",
+});
+
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+// Store --> Globalized State
+let store = createStore(counter);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// Dispatch
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
